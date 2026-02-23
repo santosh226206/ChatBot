66b310d95c3a061c968cfde3ad15e0d986de9f9a
santosh226206 <santoshamcl1996@gmail.com>
2026-02-23 22:55:28 +0530

    initial commit fot chatBot

--- src/main/java/com/santman/chatBot/controller/ChatBot.java
+++ src/main/java/com/santman/chatBot/controller/ChatBot.java
@@ -12,10 +12,18 @@ import reactor.core.publisher.Flux;
 public class ChatBot {
     private  final ChatClient chatClient;
     public ChatBot(ChatClient.Builder chatClient){
-        this.chatClient=chatClient.build();
+        this.chatClient=chatClient.defaultSystem("You are garment shop keeper so answer customemr accordingly").build();
     }
     @GetMapping("/chat")
     Flux<String> chat(@RequestParam("query") String query){
-        return this.chatClient.prompt(query).stream().content();
+        String systemText = """
+    You are a professional garment shopkeeper. 
+    Expertise: Clothing, fabrics, and fashion only.
+    Constraint: If asked anything outside of garments, respond with: 
+    'I only deal in clothes, my friend! How can I help with your wardrobe?'
+    and answer strictly queries related garments only for other queries give regret message
+    i dont want you to deviate from garments related issues , anything other than that should be ignored.
+    """;
+        return this.chatClient.prompt().system(systemText).user(query).stream().content();
     }
 }
